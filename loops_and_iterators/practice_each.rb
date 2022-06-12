408298f53c3828cf528a4f96c6b2f4e307eca797
Kiran Deck <kiranjdeck@gmail.com>
2022-06-12 14:47:17 -0400

    loops and iterators

---
+++ loops_and_iterators/practice_each.rb
@@ -0,0 +1,9 @@
+names = ["Bob", "Kevin", "Johanne", "Gale", "Marcus"]
+x = 1
+
+names.each do |name|
+  puts "#{x}. #{name}"
+  x += 1
+end
+
+
---
+++ loops_and_iterators/two.rb
@@ -0,0 +1,10 @@
+x = ""
+
+while x != "STOP"
+  puts "How are you?"
+  answer = gets.chomp
+  puts "Should i ask again?"
+  x = gets.chomp
+end
+
+
